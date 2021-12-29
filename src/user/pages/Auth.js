81c2bf0c9579dc2ece706e6e4534cdc8f902da10
Gowthami Pediredla <tech-dev@expert-works.com>
2021-12-30 00:41:22 +0530

    spinner, errormodal, login,signup api integration and index.html div id

--- src/user/pages/Auth.js
+++ src/user/pages/Auth.js
@@ -11,10 +11,14 @@ import {
 import { useForm } from '../../shared/hooks/form-hook';
 import { AuthContext } from '../../shared/context/auth-context';
 import './Auth.css';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
+import ErrorModal from "../../shared/components/UIElements/ErrorModal"
 
 const Auth = () => {
   const auth = useContext(AuthContext);
   const [isLoginMode, setIsLoginMode] = useState(true);
+  const [isLoading, setisLoading] = useState(false);
+  const [error, seterror] = useState(null)
 
   const [formState, inputHandler, setFormData] = useForm(
     {
@@ -54,14 +58,84 @@ const Auth = () => {
     setIsLoginMode(prevMode => !prevMode);
   };
 
-  const authSubmitHandler = event => {
+  const authSubmitHandler = async event => {
     event.preventDefault();
     console.log(formState.inputs);
+    setisLoading(true)
+
+    if(isLoginMode){
+      const payload={
+        email:formState.inputs.email.value,
+        pwd:formState.inputs.password.value
+
+      }
+      try{
+        const response= await fetch("http://localhost:5000/api/users/login",{
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify(payload)
+        });
+        const jsonData=await response.json();
+        if(!response.ok){
+          throw new Error(response.message)
+        }
+        debugger
+        console.log('jsondata',jsonData);
+        setisLoading(false)
     auth.login();
+
+      }
+      catch(e){
+        setisLoading(false)
+        seterror(e.message||'Something went wrong, Please try again later')
+
+
+      }
+    }
+    else{
+      const payload={
+        email:formState.inputs.email.value,
+        pwd:formState.inputs.password.value,
+        uname:formState.inputs.name.value
+  
+      }
+      try{
+        const response= await fetch("http://localhost:5000/api/users/signup",{
+          method:'POST',
+          headers:{
+            'Content-Type':'application/json'
+          },
+          body:JSON.stringify(payload)
+        });
+        const jsonData=await response.json();
+        if(!response.ok){
+          throw new Error(response.message)
+        }
+        debugger
+        console.log('jsondata',jsonData);
+        setisLoading(false)
+    auth.login();
+
+      }
+      catch(e){
+        setisLoading(false)
+        seterror(e.message||'Something went wrong, Please try again later')
+
+
+      }
+      
+    }
+    
   };
 
   return (
+    <>
+      {error&&<ErrorModal error={error} onClear={()=>seterror(null)}/>}
+
     <Card className="authentication">
+      {isLoading&&<LoadingSpinner asOverlay/>}
       <h2>Login Required</h2>
       <hr />
       <form onSubmit={authSubmitHandler}>
@@ -102,6 +176,7 @@ const Auth = () => {
         SWITCH TO {isLoginMode ? 'SIGNUP' : 'LOGIN'}
       </Button>
     </Card>
+    </>
   );
 };
 
