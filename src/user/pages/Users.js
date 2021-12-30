383a82c9e05a04463c50f046d6b38a0081832e73
Gowthami Pediredla <tech-dev@expert-works.com>
2021-12-30 23:24:06 +0530

    users api integration

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,5 +1,6 @@
-import React from 'react';
-
+import React,{useEffect,useState} from 'react';
+import ErrorModal from '../../shared/components/UIElements/ErrorModal';
+import LoadingSpinner from '../../shared/components/UIElements/LoadingSpinner';
 import UsersList from '../components/UsersList';
 
 const Users = () => {
@@ -12,8 +13,38 @@ const Users = () => {
       places: 3
     }
   ];
+  const [loadedUsers, setloadedUsers] = useState([]);
+  const [isLoading, setisLoading] = useState(false);
+  const [error, seterror] = useState(null)
+  useEffect(() => {
+    try{
+      setisLoading(true)
+      const getUsers=async()=>{
+      const response=await fetch("http://localhost:5000/api/users")
+        const responseData=await response.json();
+        debugger;
+        if(!response.ok){
+          throw new error('Somthing went wrong')
+        }
+        setloadedUsers(responseData.users)
+      }
+      getUsers();
+      setisLoading(false)
+    }
+    catch(e){
+      setisLoading(false);
+      seterror(e.message||'something went wrong, Please try again')
+    }
+   
+  }, [])
 
-  return <UsersList items={USERS} />;
+  return (
+    <>
+    <ErrorModal show={error} onCancel={()=>seterror(null)}/>
+    {isLoading&&<div className="center"><LoadingSpinner asOverlay/></div>}
+    {!isLoading&&<UsersList items={loadedUsers} />}
+    </>
+  )
 };
 
 export default Users;
